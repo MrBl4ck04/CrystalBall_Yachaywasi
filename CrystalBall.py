3a43ebec5569cda8465dd3dc6e8176ae08a651d6
MrBl4ck <franz.carvajal04@gmail.com>
2023-12-03 21:30:11 -0400

    Se corrigío el punto de reorden y la demanda

--- CrystalBall.py
+++ CrystalBall.py
@@ -2,7 +2,7 @@ import simpy
 import random
 
 class Libro:
-    def __init__(self, titulo, autor, editorial, precio, isbn, stock_inicial, demanda, punto_reorden, cantidad_ordenar):
+    def __init__(self, titulo, autor, editorial, precio, isbn, stock_inicial, demanda, tiempo_reposicion, cantidad_ordenar):
         self.titulo = titulo
         self.autor = autor
         self.editorial = editorial
@@ -10,8 +10,12 @@ class Libro:
         self.isbn = isbn
         self.stock = stock_inicial
         self.demanda = demanda
-        self.punto_reorden = punto_reorden
+        self.tiempo_reposicion = tiempo_reposicion
         self.cantidad_ordenar = cantidad_ordenar
+        self.punto_reorden = self.calcular_punto_reorden()
+
+    def calcular_punto_reorden(self):
+        return self.demanda * self.tiempo_reposicion
 
 def simulacion_libreria(env, libros):
     eventos = [env.process(ventas_libreria(env, libro)) for libro in libros]
@@ -24,21 +28,21 @@ def ventas_libreria(env, libro):
         if venta > libro.stock:
             venta = libro.stock
         libro.stock -= venta
-        print(f"[+] Venta de '{libro.titulo}': {venta}, Stock Actual: {libro.stock}")
+        print(f"Venta de '{libro.titulo}': {venta}, Stock Actual: {libro.stock}")
 
         # Reordenar si es necesario
         if libro.stock <= libro.punto_reorden:
             ordenar = libro.cantidad_ordenar
             libro.stock += ordenar
-            print(f"[+] Reordenar '{libro.titulo}': {ordenar}, Stock Actual: {libro.stock}")
+            print(f"Reordenar '{libro.titulo}': {ordenar}, Stock Actual: {libro.stock}")
 
 # Definir los datos para los 5 libros
 libros_data = [
-    {"titulo": "PEDRO PARAMO", "autor": "JUAN RULFO", "editorial": "CATEDRA", "precio": 95.00, "isbn": "9788437604183", "stock_inicial": 80, "demanda": 48, "punto_reorden": 6, "cantidad_ordenar": 12.649},
-    {"titulo": "LEER PARA INVESTIGAR", "autor": "LUIS ROBERTO AMUSQUIVAR FERNANDEZ", "editorial": "CATACORA", "precio": 23.00, "isbn": "4-1-3762-15", "stock_inicial": 49, "demanda": 48, "punto_reorden": 6, "cantidad_ordenar": 12.649},
-    {"titulo": "REBELION EN LA GRANJA", "autor": "GEORGE ORWELL", "editorial": "EDICIONES AMERICANAS", "precio": 65.00, "isbn": "9789962904915", "stock_inicial": 55, "demanda": 32, "punto_reorden": 4, "cantidad_ordenar": 10.328},
-    {"titulo": "GENERACION IDIOTA", "autor": "AGUSTIN LAJE", "editorial": "HOJAS DEL SUR", "precio": 195.00, "isbn": "9789878916347", "stock_inicial": 43, "demanda": 30, "punto_reorden": 3.75, "cantidad_ordenar": 10.000},
-    {"titulo": "48 LEYES DEL PODER, LAS", "autor": "ROBERT GREENE", "editorial": "OCEANO", "precio": 228.00, "isbn": "9786075276915", "stock_inicial": 36, "demanda": 28, "punto_reorden": 3.5, "cantidad_ordenar": 9.661}
+    {"titulo": "PEDRO PARAMO", "autor": "JUAN RULFO", "editorial": "CATEDRA", "precio": 95.00, "isbn": "9788437604183", "stock_inicial": 80, "demanda": 48, "tiempo_reposicion": 6, "cantidad_ordenar": 12.649},
+    {"titulo": "LEER PARA INVESTIGAR", "autor": "LUIS ROBERTO AMUSQUIVAR FERNANDEZ", "editorial": "CATACORA", "precio": 23.00, "isbn": "4-1-3762-15", "stock_inicial": 49, "demanda": 48, "tiempo_reposicion": 6, "cantidad_ordenar": 12.649},
+    {"titulo": "REBELION EN LA GRANJA", "autor": "GEORGE ORWELL", "editorial": "EDICIONES AMERICANAS", "precio": 65.00, "isbn": "9789962904915", "stock_inicial": 55, "demanda": 32, "tiempo_reposicion": 4, "cantidad_ordenar": 10.328},
+    {"titulo": "GENERACION IDIOTA", "autor": "AGUSTIN LAJE", "editorial": "HOJAS DEL SUR", "precio": 195.00, "isbn": "9789878916347", "stock_inicial": 43, "demanda": 30, "tiempo_reposicion": 3.75, "cantidad_ordenar": 10.000},
+    {"titulo": "48 LEYES DEL PODER, LAS", "autor": "ROBERT GREENE", "editorial": "OCEANO", "precio": 228.00, "isbn": "9786075276915", "stock_inicial": 36, "demanda": 28, "tiempo_reposicion": 3.5, "cantidad_ordenar": 9.661}
 ]
 
 # Configurar la simulación
@@ -48,3 +52,4 @@ libros = [Libro(**data) for data in libros_data]
 # Iniciar la simulación
 env.process(simulacion_libreria(env, libros))
 env.run(until=50)  # Establecer un límite de tiempo para la simulación
+
